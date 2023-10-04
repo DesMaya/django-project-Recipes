d4de64bf97464aa9cb0aef710a9af77c8d40dd1c
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-04 17:50:19 -0300

    Start the first files and remove test files

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
-
-
-def contato(request):
-    return HttpResponse("Contato")
+    return render(request, 'recipes/home.html', status=200, context={
+        'name': 'Maya Florentino',
+    })
