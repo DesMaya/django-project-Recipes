4ec3bbab105d6d275ea0ad6de3baf65285584c1d
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-09 20:07:45 -0300

    collecting static files

--- projeto/settings.py
+++ projeto/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 
+# Create the STATIC_ROOT to collect all static files and put in this folder.
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
