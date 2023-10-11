e2ec9191591f52ebd7de23a242e05d8cfb54c547
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-10 21:02:50 -0300

    inject fake data for recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from utils.recipes.data_generator import make_recipe
 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=200, context={
-        'name': 'Maya Florentino',
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe()
+    })
