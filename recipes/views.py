6348460dc01684fd792489630716ebdaac3e9e4c
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-11 19:10:44 -0300

    set dynamic content for detail page

--- recipes/views.py
+++ recipes/views.py
@@ -13,5 +13,6 @@ def home(request):
 
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe()
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })
