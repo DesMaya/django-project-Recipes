ff5bb6c231567cecf548638cfe56973c23bac920
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-11 19:55:11 -0300

    dynamic url with id for recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes)
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipes, name="recipes-recipe")
 ]
