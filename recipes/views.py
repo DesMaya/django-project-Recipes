5dbc5cf3a38d628cfff195b313e6e280c868f14a
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-16 18:15:17 -0300

    filter to show only published recipes, and add a page for category

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from utils.recipes.data_generator import make_recipe
+# from utils.recipes.data_generator import make_recipe
 from .models import Recipe
 
 
@@ -7,15 +7,18 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id)
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
