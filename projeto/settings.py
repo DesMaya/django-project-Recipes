b636636121502b4c7ab85776b87d472a6468cb69
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-16 10:13:36 -0300

    set the configuration to media

--- projeto/settings.py
+++ projeto/settings.py
@@ -133,6 +133,10 @@ STATICFILES_DIRS = [
 # Create the STATIC_ROOT to collect all static files and put in this folder.
 STATIC_ROOT = BASE_DIR / 'static'
 
+# Show to django where we want to users media.
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
