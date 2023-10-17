28239ca86ac43a2df411b23ae3e427cbd04d681f
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-17 20:29:01 -0300

    config the pytest and pytest.ini

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        print("Olá Mundo!")
+        assert 1 == 1
+        ...
