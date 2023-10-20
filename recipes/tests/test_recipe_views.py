12da5f97a53a72d06bed8713520eb7b54ae49872
Maya Florentino da Silva <mayaflorentinodasilva@gmail.com>
2023-10-20 18:32:48 -0300

    created a test to check if recipe data is loaded in template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,9 +1,19 @@
-from django.test import TestCase
-from django.urls import reverse, resolve
+from .test_recipe_base import RecipeTestBase
+from django.urls import reverse, resolve  # type: ignore
 from recipes import views
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+
+    # Esse método é executado sempre antes de qualquer
+    # função dentro dessa classe
+    def setUp(self) -> None:
+        return super().setUp()
+
+    # Esse método é executado sempre depois de qualquer
+    # função dentro dessa classe
+    def tearDown(self) -> None:
+        return super().tearDown()
 
     # Home tests ---------------------------------------------------------
 
@@ -24,6 +34,9 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipes_home_template_shows_no_recipes_found_if_no_recipes(self):
+        # É possível deletar o model criado apenas para esse test
+        # Recipe.objects.get(pk=1).delete()
+
         response = self.client.get(
             reverse('recipes:home')
         )
@@ -31,6 +44,33 @@ class RecipeViewsTest(TestCase):
             "No Recipes!",
             response.content.decode(response.charset)
         )
+
+    def test_recipes_home_template_loads_recipes(self):
+        self.make_recipe(
+            author_data={
+                'first_name': 'Maya'
+            },
+            category_data={
+                'name': 'Café da Manhã'
+            },
+            title='New Recipe Title',
+            preparation_time=15
+        )
+
+        response = self.client.get(
+            reverse('recipes:home'),
+        )
+        response_recipes = response.context['recipes']
+        recipe = response_recipes.first()
+        self.assertEqual(len(response_recipes), 1)
+        self.assertEqual(recipe.title, 'New Recipe Title')
+
+        content = response.content.decode(response.charset)
+        self.assertIn('New Recipe Title', content)
+        self.assertIn('15 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertIn('Café da Manhã', content)
+        self.assertIn('Maya', content)
 
     # Category Tests ---------------------------------------------------------
 
@@ -47,7 +87,7 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(
             reverse(
                 'recipes:category',
-                kwargs={'category_id': 1}
+                kwargs={'category_id': 1000}
             )
         )
         self.assertEqual(response.status_code, 404)
@@ -67,7 +107,7 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(
             reverse(
                 'recipes:recipe',
-                kwargs={'id': 1}
+                kwargs={'id': 1000}
             )
         )
         self.assertEqual(response.status_code, 404)
